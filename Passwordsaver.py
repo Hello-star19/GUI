a18ba822ad1b4fcb3cf0350be5cee18c5309ff53
Hello-star19 <mirza.mahid@gmail.com>
2026-02-03 16:31:18 +0000

    Update Passwordsaver.py

--- Passwordsaver.py
+++ Passwordsaver.py
@@ -1,8 +1,15 @@
 from tkinter import*
 
 screen = Tk()
-screen.geometry("450x500")
+screen.geometry("450x350")
 screen.config(background="white")
+def add():
+    usernames = entry_box1.get()
+    passwords = entry_box2.get()
+    opening = open("logins.txt","a")
+    opening.write(f"\nUsername:{usernames} Password:{passwords}")
+    entry_box1.delete(0,END)
+    entry_box2.delete(0,END)
 
 username = Label(screen, text = "Username", foreground= "black", font=("Calibri"))
 username.place(x = 70, y=100)
@@ -13,10 +20,11 @@ password.place( x= 70, y= 150)
 entry_box1 = Entry(screen,width = 30 )
 entry_box1.place( x= 165, y = 105) 
 
-entry_box2 = Entry(screen,width = 30 )
+entry_box2 = Entry(screen,show= "*",width = 30 )
 entry_box2.place( x= 165, y = 155) 
 
-submit_button = Button(screen, text = "Submit", foreground= "White", font=("Arial"), background="Navy")
+submit_button = Button(screen, text = "Submit", foreground= "White", font=("Arial"), background="Navy",command=add)
 submit_button.place(x= 70, y=200)
+
 
 screen.mainloop()
